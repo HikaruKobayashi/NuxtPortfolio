527756b94f772286051eee8828157c050872b203
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-06 16:14:33 +0900

    ハンバーガーメニューを作成する。

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class='header'>
-    <h1>Sample</h1>
-    <ul class="menu">
-      <li>メニュー</li>
-    </ul>
+  <div class='header-logo-menu menu'>
+    <div id='nav-drawer'>
+      <input  id="nav-input" type="checkbox" class="nav-unshown"/>
+      <label id="nav-open" for="nav-input"><span></span></label>
+      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
+      <div id="nav-content">ここに中身を入れる</div>
+    </div>
   </div>
 </template>
 
