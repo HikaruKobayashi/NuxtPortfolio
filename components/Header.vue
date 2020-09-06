81d433c7c8ffea905d17d49f3447fe391a72fdb4
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-06 16:01:48 +0900

    ヘッダーテンプレートを作成した。

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class='header'>
+    <h1>Sample</h1>
+    <ul class="menu">
+      <li>メニュー</li>
+    </ul>
+  </div>
+</template>
+
+<style src='assets/css/main.css' />
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
+    <Header />
     <Nuxt />
   </div>
 </template>
 
+<script>
+import Header from '~/components/Header.vue'
+
+export default {
+  name: 'sample',
+  components: {
+    Header
+  }
+}
+</script>
 <style src='assets/css/main.css' />
