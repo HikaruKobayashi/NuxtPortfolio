87822e802fce574a40370cc21a8497f67f528b06
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-07 15:01:08 +0900

    導入部分の背景色などを整える。

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -4,14 +4,14 @@
       <h1>Portfolio</h1>
       <span>This page is a my self-introduction site.</span>
     </div>
-    <div class='img_cotainer'>
-      <img src='@/assets/img/nature.jpg' class='introduction_img nature' />
+    <div>
+      <img src='@/assets/img/work-environment.jpg' class='introduction_img work-environment' />
     </div>
-    <div class='img_cotainer'>
-      <img src='@/assets/img/natural.jpg' class='introduction_img natural' />
+    <div>
+      <img src='@/assets/img/swimming.jpg' class='introduction_img swimming' />
     </div>
-    <div class='img_cotainer'>
-      <img src='@/assets/img/nature.jpg' class='introduction_img nature-2' />
+    <div>
+      <img src='@/assets/img/dive.jpg' class='introduction_img dive' />
     </div>
   </div>
 </template>
