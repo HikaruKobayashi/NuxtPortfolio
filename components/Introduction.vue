6c5c9e9ae2f2050b18f964a634f1749b8f2ce5e3
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-08 16:54:40 +0900

    CSSをできる限りまとめる。

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,23 +1,23 @@
 <template>
   <div class='introduction'>
-    <div class='site-title'>
+    <div class='site_title'>
       <h1>Portfolio</h1>
       <span>This page is a my self-introduction site.</span>
     </div>
     <div class='introduction_img_fade_out' v-scroll="handleScrollFadeOut">
-      <img src='@/assets/img/work-environment.jpg' class='introduction_img work-environment' />
+      <img src='@/assets/img/work-environment.jpg' class='introduction_img introduction_img_work-environment' />
     </div>
     <div class='introduction_img_fade_out' v-scroll="handleScrollFadeOut">
-      <img src='@/assets/img/swimming.jpg' class='introduction_img swimming' />
+      <img src='@/assets/img/swimming.jpg' class='introduction_img introduction_img_swimming' />
     </div>
     <div class='introduction_img_fade_out' v-scroll="handleScrollFadeOut">
-      <img src='@/assets/img/dive.jpg' class='introduction_img dive' />
+      <img src='@/assets/img/dive.jpg' class='introduction_img introduction_img_dive' />
     </div>
     <div class='introduction_img_fade_out' v-scroll="handleScrollFadeOut">
-      <img src='@/assets/img/outdoors.png' class='introduction_img outdoors' />
+      <img src='@/assets/img/outdoors.png' class='introduction_img introduction_img_outdoors' />
     </div>
     <div class='introduction_img_fade_out' v-scroll="handleScrollFadeOut">
-      <img src='@/assets/img/riverbank.png' class='introduction_img river_bank' />
+      <img src='@/assets/img/riverbank.png' class='introduction_img introduction_img_river_bank' />
     </div>
   </div>
 </template>
