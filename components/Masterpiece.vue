f355ad4d427c1fa787efb9799b167615a3e4fd23
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-08 15:25:59 +0900

    画像を中央に集める。

--- components/Masterpiece.vue
+++ components/Masterpiece.vue
@@ -1,6 +1,13 @@
 <template>
   <div class='masterpiece'>
-    <h1>The Works</h1>
+    <div class='masterpiece_title'>
+      <h1>The Works</h1>
+    </div>
+    <div class='Ringo'>
+      <h2>Ringo</h2>
+      <p>Ringo is an application that allows you to talk to Ringo Shiina on LINE.</p>
+      <p>It can be achieved by using Ruby on Rails and LINE API.</p>
+    </div>
   </div>
 </template>
 
