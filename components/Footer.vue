819d97edb90e7832710faf35de80b60a2e1bf158
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-11 22:24:57 +0900

    使っていないコンポーネントを削除する。

--- components/Footer.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <footer class='footer'>
-    <p>&copy; Hikaru Kobayashi</p>
-  </footer>
-</template>
-
-<style src='assets/css/main.css' />
--- components/contact.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class='container'>
-    <div>
-      <h1>Contact</h1>
-    </div>
-  </div>
-</template>
-
-<style src='assets/css/main.css' />
