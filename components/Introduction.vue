6ca80f3e1f1864ea5b405c24be840632a21225e4
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-12 14:52:55 +0900

    いらない処理を削除する。

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -13,8 +13,6 @@ $(document).ready(function() {
    $(window).on('scroll', function() {
     if($(window).scrollTop() < 1000) {
       $('.hero').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
-      $('.hero h1').css('top', 50 + ($(window).scrollTop() * .1) + '%');
-      $('.hero h1').css('opacity', 1 - ($(window).scrollTop() * .003));
     }
   });
 });
