83673df61d9290ac803048d7e7c97d5f87be5067
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-13 18:38:58 +0900

    最低限のスタイリングを追加する。

--- components/Contact.vue
+++ components/Contact.vue
@@ -5,21 +5,25 @@
 
       <div class="p-contact__item">
         <label for="username">お名前</label>
+        <br>
         <input type="text" id="username" name="username" v-model="username" autocomplete="name">
       </div>
 
       <div class="p-contact__item">
         <label for="katakana">フリガナ</label>
+        <br>
         <input type="text" id="katakana" name="katakana" v-model="katakana">
       </div>
 
       <div class="p-contact__item">
         <label for="useremail">メールアドレス</label>
+        <br>
         <input type="text" id="useremail" name="useremail" v-model="useremail" autocomplete="email">
       </div>
 
       <div class="p-contact__item">
         <label for="message">お問い合わせ内容</label>
+        <br>
         <textarea id="message" name="message" v-model="message"></textarea>
       </div>
 
@@ -29,7 +33,7 @@
       </div>
 
       <div class="p-contact__submit">
-        <button type="submit">送信</button>
+        <button type="submit" class='btn btn-dark'>送信</button>
       </div>
     </form>
   </div>
@@ -40,11 +44,18 @@
   .contact {
     height: 50vh;
     background-color: rgb(241,243,244);
+    position: relative;
   }
 
   .contact_form {
     width: 50%;
     text-align: center;
-    margin: auto;
+    background-color: rgb(255, 255, 255);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
   }
 </style>
