cfd4cadd3760b0de624303fcd0aaae45dae864e9
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-13 15:41:16 +0900

    コンポーネント単位でまとめる。

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,7 +1,7 @@
 <template>
   <div class='introduction'>
-    <div class="hero"></div>
-    <div class='site_title'>
+    <div class="introduction_bg_img"></div>
+    <div class='introduction_title'>
       <h1>Portfolio</h1>
       <span>This page is a my self-introduction site.</span>
     </div>
@@ -12,8 +12,39 @@
 $(document).ready(function() {
    $(window).on('scroll', function() {
     if($(window).scrollTop() < 1000) {
-      $('.hero').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
+      $('.introduction_bg_img').css('background-size', 130 + parseInt($(window).scrollTop() / 5) + '%');
     }
   });
 });
 </script>
+
+<style>
+  .introduction {
+    height: 100vh;
+  }
+
+  .introduction_title {
+    padding: 80px;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -webkit-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    background: linear-gradient(rgb(52,62,98), rgb(24,26,41));
+    color: rgb(255, 255, 255);
+    font-weight: bold;
+  }
+
+  .introduction_bg_img {
+    position: absolute;
+    width: 100%;
+    height: 100vh;
+    background-color: rgb(41,41,48);
+    background-image: url(~@/assets/img/landscape.jpg);
+    background-repeat: no-repeat;
+    background-size: 130%;
+    overflow: hidden;
+  }
+</style>
