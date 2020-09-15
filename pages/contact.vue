b95e9b8bd60df6e9e8db4e6142378ad880db29f7
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-15 17:19:16 +0900

    ダミーを用意する。

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,6 +1,5 @@
 <template>
-  <client-only>
-  <form id="contact" action="thanks" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+  <form id="contact" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
     <input type="hidden" name="form-name" value="contact">
     <p>
       <label>Your Name: <input type="text" name="name" required="true"></label>   
@@ -15,16 +14,4 @@
       <button type="submit">Send</button>
     </p>
   </form>
-  </client-only>
 </template>
-
-<script>
-  $("#contact").submit(function(e) {
-    e.preventDefault();
-
-    var $form = $(this);
-    $.post($form.attr("action"), $form.serialize()).then(function() {
-      alert("Thank you!");
-    });
-  });
-</script>
--- pages/thanks.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Thanks</h1>
-</template>
