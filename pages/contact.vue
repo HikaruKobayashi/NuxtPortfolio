5c25968306c594300188404e837719a23d3e0568
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-15 17:04:05 +0900

    アクションを追加する。

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,15 +1,15 @@
 <template>
   <client-only>
-  <form id="contact" name="contact" method="POST" data-netlify="true">
+  <form id="contact" action="thanks" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
     <input type="hidden" name="form-name" value="contact">
     <p>
-      <label>Your Name: <input type="text" name="name"></label>   
+      <label>Your Name: <input type="text" name="name" required="true"></label>   
     </p>
     <p>
-      <label>Your Email: <input type="email" name="email"></label>
+      <label>Your Email: <input type="email" name="email" required="true"></label>
     </p>
     <p>
-      <label>Message: <textarea name="message"></textarea></label>
+      <label>Message: <textarea name="message" required="true"></textarea></label>
     </p>
     <p>
       <button type="submit">Send</button>
