0870bb846730cb97d11deffbcd63ca2c74205f45
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-15 23:54:13 +0900

    Ajaxでフォームの内容をsubmitする。

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <form id="contact" name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+  <form id="contact" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
     <input type="hidden" name="form-name" value="contact">
     <p>
       <label>Your Name: <input type="text" name="name" required="true"></label>   
@@ -15,3 +15,34 @@
     </p>
   </form>
 </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      name: '',
+      email: '',
+      message: '',
+      isSubmit: false
+    }
+  },
+  methods: {
+    onSubmit() {
+      const params = new URLSearchParams()
+
+      params.append('form-name', 'contact')
+      params.append('name', this.name)
+      params.append('email', this.email)
+      params.append('message', this.content)
+
+      axios
+        .post('/', params)
+        .then(() => {
+          this.isSubmit = true
+        })
+    }
+  }
+}
+</script>
