d97075875550f2768a85a81b3eb0bfecbb02dd8a
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-16 00:15:52 +0900

    submit後にトップへ遷移させる。

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <form id="contact" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
+  <form id="contact" action="/" name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field">
     <input type="hidden" name="form-name" value="contact">
     <p>
       <label>Your Name: <input type="text" name="name" required="true"></label>   
