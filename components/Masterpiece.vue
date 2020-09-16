3a0a75882ce73316ab2b7921b468d5a5cc604e25
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-16 14:30:30 +0900

    外部リンクを追加する。

--- components/Masterpiece.vue
+++ components/Masterpiece.vue
@@ -6,6 +6,7 @@
         <p>Money Hub is a household account book application that makes money in and out simpler.</p>
         <p>By subdividing daily money transfer into and out of income, savings, fixed costs, and variable costs, </p>
         <p>you can easily get information such as where you are spending most of your money and how many assets you currently have.</p>
+        <a href="https://github.com/HikaruKobayashi/MoneyHub">Git Hub</a>
       </div>
     </div>
     <div data-aos='fade-up'>
@@ -14,6 +15,7 @@
         <p>Ringo makes it possible to do LINE with the person you admire.</p>
         <p>You can create the content of the reply yourself.</p>
         <p>Also, if you use Heroku's schedule function, LINE will be sent at a fixed time every day.</p>
+        <a href="https://github.com/HikaruKobayashi/Ringo">Git Hub</a>
       </div>
     </div>
     <div data-aos='fade-up'>
@@ -22,6 +24,7 @@
         <p>NuxtPortfolio is this site.This site is my self-introduction site created using Nuxt.js.</p>
         <p>I'm using SPA mode in Nuxt.js. I'm deploying with Netlify.</p>
         <p>I hope you understand me through this site.</p>
+        <a href="https://github.com/HikaruKobayashi/NuxtPortfolio">Git Hub</a>
       </div>
     </div>
   </div>
