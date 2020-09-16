e78b3dca679da575acc08dc608eed575c7ce0815
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-09-16 10:51:25 +0900

    作品の説明箇所を書き換える。

--- components/Masterpiece.vue
+++ components/Masterpiece.vue
@@ -4,21 +4,24 @@
       <h1 class='masterpiece_title'>Money Hub</h1>
       <div class='masterpiece_content'>
         <p>Money Hub is a household account book application that makes money in and out simpler.</p>
-        <p>By subdividing daily money transfer into and out of income, savings, fixed costs, and variable costs, you can easily get information such as where you are spending most of your money and how many assets you currently have.</p>
+        <p>By subdividing daily money transfer into and out of income, savings, fixed costs, and variable costs, </p>
+        <p>you can easily get information such as where you are spending most of your money and how many assets you currently have.</p>
       </div>
     </div>
     <div data-aos='fade-up'>
-      <h1 class='masterpiece_title'>Money Hub</h1>
+      <h1 class='masterpiece_title'>Ringo</h1>
       <div class='masterpiece_content'>
-        <p>Money Hub is a household account book application that makes money in and out simpler.</p>
-        <p>By subdividing daily money transfer into and out of income, savings, fixed costs, and variable costs, you can easily get information such as where you are spending most of your money and how many assets you currently have.</p>
+        <p>Ringo makes it possible to do LINE with the person you admire.</p>
+        <p>You can create the content of the reply yourself.</p>
+        <p>Also, if you use Heroku's schedule function, LINE will be sent at a fixed time every day.</p>
       </div>
     </div>
     <div data-aos='fade-up'>
-      <h1 class='masterpiece_title'>Money Hub</h1>
+      <h1 class='masterpiece_title'>NuxtPortfolio</h1>
       <div class='masterpiece_content'>
-        <p>Money Hub is a household account book application that makes money in and out simpler.</p>
-        <p>By subdividing daily money transfer into and out of income, savings, fixed costs, and variable costs, you can easily get information such as where you are spending most of your money and how many assets you currently have.</p>
+        <p>NuxtPortfolio is this site.This site is my self-introduction site created using Nuxt.js.</p>
+        <p>I'm using SPA mode in Nuxt.js. I'm deploying with Netlify.</p>
+        <p>I hope you understand me through this site.</p>
       </div>
     </div>
   </div>
