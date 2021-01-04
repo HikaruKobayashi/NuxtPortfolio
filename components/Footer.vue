8f7af224b75cd5f71d4579e79288f27029426642
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-01-04 12:00:00 +0900

    フッターを作成する。

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+  <footer id="footer">
+    <p>2021@Hikaru</p>
+  </footer>
+</template>
+
+<style>
+  #footer {
+    width: 100%;
+    text-align: center;
+    border-bottom: 20px solid rgb(17, 32, 63);
+  }
+  #footer > p {
+    margin: 50px 0;
+  }
+</style>
