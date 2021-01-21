ef08be72d20e9eb97a7c6119b4a84792f1429273
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-01-22 00:18:21 +0900

    トップへ戻るボタンをコンポーネント化する。

---
+++ components/ScrollBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="Page-Btn" v-scroll-to="'#app'" to>
+      <fa class="Page-Btn-Icon" :icon="['fas', 'chevron-up']" area-hidden="true" />
+    </div>
+</template>
+
+<style>
+  .Page-Btn{
+    position: fixed;
+    right: 14px;
+    bottom: 14px;
+    width: 45px;
+    height: 45px;
+    line-height: 45px;
+    text-align: center;
+    border-radius: 50%;
+    background: linear-gradient(rgb(52,62,98), rgb(24,26,41));
+  }
+
+  .Page-Btn-Icon{
+    color: #fff;
+    font-size: 16px;
+  }
+</style>
