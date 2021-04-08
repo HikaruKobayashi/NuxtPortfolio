a59e27e8adfb8548d37dc8d3abdfe5301cc15fd3
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2021-04-09 08:01:39 +0900

    Try Tailwind css

--- components/Introduction.vue
+++ components/Introduction.vue
@@ -1,10 +1,10 @@
 <template>
   <section id="introduction">
-    <div class="inner">
+    <div class="inner text-center w-3/6 text-white mx-auto my-0">
       <h1
         v-for="(t, index) in title"
         :key="index"
-        class="item"
+        class="item font-bold text-4xl inline-block"
         :style="{animationDelay: index*100+'ms'}"
         v-text="t"
       >{{ title }}</h1>
@@ -22,22 +22,15 @@ export default class introduction extends Vue {
 }
 </script>
 
-<style lang="sass">
+<style lang="sass" scoped>
 #introduction
   width: 100%
-  padding: 200px 0 200px 0
-  text-align: center
+  padding: 200px 0
   background: url(../assets/img/city.jpg) no-repeat
   background-size: cover
   .inner
-    width: 1000px
-    margin: 0 auto
-    color: rgb(255, 255, 255)
-    // background: linear-gradient(rgb(52,62,98), rgb(24,26,41))
     padding: 100px 0
-
 .item
-  display: inline-block
   min-width: 0.3em
   animation: text-in .8s cubic-bezier(0.22, 0.15, 0.25, 1.43) 0s backwards
 
